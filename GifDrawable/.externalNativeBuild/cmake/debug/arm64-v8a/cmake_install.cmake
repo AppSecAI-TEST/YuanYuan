58ca8692804ee4eace1686147bdeb90c62e629e7
Nathaniel <nathanwriting@126.com>
2017-07-29 22:16:58 +0800

    编译之后提交

--- GifDrawable/.externalNativeBuild/cmake/debug/arm64-v8a/cmake_install.cmake
+++ GifDrawable/.externalNativeBuild/cmake/debug/arm64-v8a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/Workspace/CommonUtil/GifDrawable/src/main/c
+# Install script for directory: E:/AndroidStudio/CommonUtil/GifDrawable/src/main/c
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/Workspace/CommonUtil/GifDrawable/.externalNativeBuild/cmake/debug/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/AndroidStudio/CommonUtil/GifDrawable/.externalNativeBuild/cmake/debug/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- GifDrawable/.externalNativeBuild/cmake/release/x86/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ GifDrawable/.externalNativeBuild/cmake/release/x86/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Windows")
 set(CMAKE_HOST_SYSTEM_VERSION "10.0.15063")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
-include("E:/AndroidSDK/ndk-bundle/build/cmake/android.toolchain.cmake")
+include("F:/AndroidSDK/ndk-bundle/build/cmake/android.toolchain.cmake")
 
 set(CMAKE_SYSTEM "Android-15")
 set(CMAKE_SYSTEM_NAME "Android")
